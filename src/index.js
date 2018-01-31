c0e69dc11ca0399ad6c872a322e22deffb3907f4
Jason Figueroa <jason.figueroa2@gmail.com>
2018-01-30 19:48:52 -0600

    added searchbar, left off on video 15

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import '../style/style.css';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyAbFrjRhm9vxUthU2CH2TIUL0mrv11FXgU';
 
 const App = () => {
-	return <div>Hello, World!!</div>
+	return(
+		<div>
+			<SearchBar />
+		</div>
+	)
 };
 
 const container = document.querySelector('.container');
